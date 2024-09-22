12dd61bc2d13064f73a7c13d645f599cb09c3d8b
Gaurav Kharal <gauravkharal9@gmail.com>
2024-09-22 22:29:06 +0545

    setting up database

---
+++ server/src/db/index.js
@@ -0,0 +1,21 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+
+const pool =  mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise();
+
+const connectDB = async () => {
+    try {
+        
+    } catch (error) {
+        console.log("Database Connection Error: ", error);
+        process.exit(1);
+    }
+}
+
+export { pool, connectDB };
