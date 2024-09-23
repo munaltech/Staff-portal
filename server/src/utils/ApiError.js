a84286271100b6d6274d29f1b5c9ef3eb5406065
gauravkharal <gauravkharal9@gmail.com>
2024-09-23 13:50:28 +0545

    prettier formatting

--- server/src/utils/ApiError.js
+++ server/src/utils/ApiError.js
@@ -1,25 +1,23 @@
 class ApiError extends Error {
-    constructor(
-        statusCode,
-        message="Internal Server Error",
-        errors =[],
-        stack =""
+  constructor(
+    statusCode,
+    message = 'Internal Server Error',
+    errors = [],
+    stack = ''
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
 
-    ){
-        super(message);
-        this.statusCode = statusCode;
-        this.data = null;
-        this.message = message;
-        this.success = false;
-        this.errors = errors;
-        
-        if(stack){
-            this.stack = stack;
-        }else{
-            Error.captureStackTrace(this, this.constructor);
-        }
-
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
 
-export {ApiError}
+export { ApiError };
