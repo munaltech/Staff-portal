b3fcd48cbfe1aeac83f9e62d3fc6316c751d2c9e
gauravkharal <gauravkharal9@gmail.com>
2024-09-23 13:49:30 +0545

    turned raw sql to sequelize

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,24 +1,24 @@
-import mysql from "mysql2";
+import { Sequelize } from 'sequelize';
 
-
-const pool =  mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}).promise();
+const sequelize = new Sequelize(
+    process.env.MYSQL_DATABASE, 
+    process.env.MYSQL_USER,     
+    process.env.MYSQL_PASSWORD, 
+    {
+        host: process.env.MYSQL_HOST,
+        dialect: 'mysql',
+        logging: false, 
+    }
+);
 
 const connectDB = async () => {
-
-    // connect to database
     try {
-        await pool.getConnection();
-        console.log("Database Connected");
-        
+        await sequelize.authenticate();
+        console.log('Database Connected');
     } catch (error) {
-        console.log("Database Connection Error: ", error);
+        console.error('Database Connection Error: ', error);
         process.exit(1);
     }
 }
 
-export { pool, connectDB };
+export { sequelize, connectDB };
