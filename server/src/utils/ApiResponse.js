f699b5a09462d4902ffad2b8b7320f014fd98194
gauravkharal <gauravkharal9@gmail.com>
2024-09-23 11:15:30 +0545

    custom response handler

---
+++ server/src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiRespose{
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.success = statusCode < 400
+        this.message = message
+    }
+}
+
+export {ApiRespose}
