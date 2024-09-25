2b726c3a7ddb8ca154e6778090cf0c39480b93d7
gauravkharal <gauravkharal9@gmail.com>
2024-09-25 16:28:25 +0545

    user crud operations introduced

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import { createUser, getUsers } from "../controllers/user.controller.js";
+import {
+    createUser,
+    updateUser,
+    getUsers,
+    deleteUser,
+    fetchUser,
+} from "../controllers/user.controller.js";
 
 const router = Router();
 
-
-
 router.route("/create").post(createUser);
+router.route("/update/:id").put(updateUser);
 router.route("/").get(getUsers);
+router.route("/:id").delete(deleteUser);
+router.route("/:id").get(fetchUser);
 
 export default router;
