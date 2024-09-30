8693e0cb65e80fca1c8e2fdf17c2a4439fd375ea
gauravkharal <gauravkharal9@gmail.com>
2024-09-30 11:41:57 +0545

    minor fix

--- client/src/components/ui/Button.jsx
+++ client/src/components/ui/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ icon, text, className, onClick, iconClass, type, disabled }) =
   return (
     <button
       className={cn(
-        "flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 active:ring-1 active:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center",
+        "flex items-center justify-center gap-2 text-white bg-blue-700 hover:bg-blue-800 active:ring-1 active:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center",
         className
       )}
       onClick={onClick}
